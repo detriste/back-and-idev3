0a5e40935a59441edddebcdd3ab443eda45b7426
detriste <detriste2018@gmail.com>
2025-02-18 08:56:25 -0300

    no

--- aula3/userService.js
+++ aula3/userService.js
@@ -3,6 +3,11 @@ const user = require("./user")
 class userService{
     constructor(){
         this.users = []; // array para armazenar
-        this.nertid;
+        this.nextid;
+    }
+    adduser(nome,email){
+        const user = new user(this.nextid++,);
+        this.users.push(user);
+        return user;
     }
 }
