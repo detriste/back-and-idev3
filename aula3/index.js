b308156866ea64f5049c8024a34b2b80f1ecb10c
detriste <detriste2018@gmail.com>
2025-03-18 09:29:31 -0300

    d

--- aula3/index.js
+++ aula3/index.js
@@ -1,26 +1,24 @@
-const express = require('express')
+const express = require('express');
 const userService = require('./userService');
 
 const app = express(); //nome qualquer para express
 app.use(express.json());
- 
 
-app.post("/users", (req, res,) => {
-    const { nome, email,senha,cpf,telefone } = req.body;
-    if (!nome || !email|| !senha ||!cpf ||!telefone){
-        return res.status(400).json
-            ({ error: "nome e email são obrigátorios" })
+app.post("/users", (req, res) => {
+    const { nome, email, senha, cpf, telefone } = req.body;
+    if (!nome || !email || !senha || !cpf || !telefone) {
+        return res.status(400).json({ error: "Todos os campos são obrigatórios: nome, email, senha, cpf, telefone" });
     }
-    const user = userService.adduser(nome, email,senha,cpf,telefone);
+    const user = userService.addUser(nome, email, senha, cpf, telefone);
     res.status(200).json({ user });
-})
-// rota pra listar os usuarios
+});
 
+// rota pra listar os usuarios
 app.get("/users", (req, res) => {
-    res.json(userService.getusers());
-})
+    res.json(userService.getUsers());
+});
 
 const port = 3000;
 app.listen(port, () => {
     console.log("servidor rodando na porta", port);
-})
+});
--- aula3/user.js
+++ aula3/user.js
@@ -1,11 +1,11 @@
 class User {
-    constructor(id, nome, email, senha) {
+    constructor(id, nome, email, senha, telefone, cpf) {
         this.id = id; // id do usuário
         this.nome = nome; // nome do usuário
         this.email = email; // e-mail do usuário
         this.senha = senha; // senha do usuário
-        this.telefone = telefone;
-        this.cpf = cpf;
+        this.telefone = telefone; // telefone do usuário
+        this.cpf = cpf; // cpf do usuário
     }
 }
 
