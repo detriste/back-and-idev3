a24302ceab4a58b97d8fafa54274a7882fc6be58
detriste <detriste2018@gmail.com>
2025-03-18 09:34:17 -0300

    goku

--- aula3/user.js
+++ aula3/user.js
@@ -4,8 +4,9 @@ class User {
         this.nome = nome; // nome do usuário
         this.email = email; // e-mail do usuário
         this.senha = senha; // senha do usuário
-        this.telefone = telefone; // telefone do usuário
         this.cpf = cpf; // cpf do usuário
+        this.telefone = telefone; // telefone do usuário
+        
     }
 }
 
