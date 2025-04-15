d65262aa4038e2017470a2311718624c59d93e31
detriste <detriste2018@gmail.com>
2025-04-15 10:52:12 -0300

    s

--- aula3/index.js
+++ aula3/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const userService = require('./userService')
+const userService = require('./userServer');
 
 const app = express() //nome pro express, pode ser qualquer coisa
 app.use(express.json()) //ativa o json no express
